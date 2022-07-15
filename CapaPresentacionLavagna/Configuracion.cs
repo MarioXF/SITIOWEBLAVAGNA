a8ca727d6844f96032fa04b7476b3a68ce8d6a10
MarioXF <shumaqrupay1@gmail.com>
2022-07-15 15:46:01 -0500

    Initial commit

---
+++ CapaPresentacionLavagna/Configuracion.cs
@@ -0,0 +1,13 @@
+﻿using CapaModelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CapaPresentacionLavagna
+{
+    public class Configuracion
+    {
+        public static Usuario oUsuario { get; set; }
+    }
+}
